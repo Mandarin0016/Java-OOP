58e7ebf16275e4b94b391cd2ba4b30abb460bee8
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-09 17:42:52 +0300

    Inheritance - Exercises

---
+++ Inheritance - Exercises/src/person/Person.java
@@ -0,0 +1,27 @@
+package person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+}
