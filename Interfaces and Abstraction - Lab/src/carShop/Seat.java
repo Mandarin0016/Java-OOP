937659ab9e999a353338b24ab59fcda610bb1a14
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-10 19:48:29 +0300

    Interfaces and Abstraction - Lab

---
+++ Interfaces and Abstraction - Lab/src/carShop/Seat.java
@@ -0,0 +1,57 @@
+package carShop;
+
+public class Seat implements Car {
+
+    private String model;
+    private String color;
+    private Integer horsePower;
+    private String countryProduced;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+        setModel(model);
+        setColor(color);
+        setHorsePower(horsePower);
+        setCountryProduced(countryProduced);
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public Integer getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return this.countryProduced;
+    }
+
+    private void setModel(String model) {
+        this.model = model;
+    }
+
+    private void setColor(String color) {
+        this.color = color;
+    }
+
+    private void setHorsePower(Integer horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    private void setCountryProduced(String countryProduced) {
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires", this.model, this.countryProduced, TIRES);
+    }
+}
