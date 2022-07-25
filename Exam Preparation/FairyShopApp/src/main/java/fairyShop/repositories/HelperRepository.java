e4ca858f727bf4b698f362930aed282e21fc8a19
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-25 23:48:17 +0300

    Exam Preparation

---
+++ Exam Preparation/FairyShopApp/src/main/java/fairyShop/repositories/HelperRepository.java
@@ -0,0 +1,41 @@
+package fairyShop.repositories;
+
+import fairyShop.models.Helper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class HelperRepository implements Repository<Helper> {
+
+    private Collection<Helper> helpers;
+
+    public HelperRepository() {
+        this.helpers = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Helper> getModels() {
+        return Collections.unmodifiableCollection(this.helpers);
+    }
+
+    @Override
+    public void add(Helper model) {
+        this.helpers.add(model);
+    }
+
+    @Override
+    public boolean remove(Helper model) {
+        return this.helpers.remove(model);
+    }
+
+    @Override
+    public Helper findByName(String name) {
+        for (Helper helper : this.helpers) {
+            if (helper.getName().equals(name)){
+                return helper;
+            }
+        }
+        return null;
+    }
+}
---
+++ Exam Preparation/FairyShopApp/src/main/java/fairyShop/repositories/PresentRepository.java
@@ -0,0 +1,41 @@
+package fairyShop.repositories;
+
+import fairyShop.models.Present;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class PresentRepository implements Repository<Present> {
+
+    private Collection<Present> presents;
+
+    public PresentRepository() {
+        this.presents = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<Present> getModels() {
+        return Collections.unmodifiableCollection(this.presents);
+    }
+
+    @Override
+    public void add(Present model) {
+        this.presents.add(model);
+    }
+
+    @Override
+    public boolean remove(Present model) {
+        return this.presents.remove(model);
+    }
+
+    @Override
+    public Present findByName(String name) {
+        for (Present present : this.presents) {
+            if (present.getName().equals(name)) {
+                return present;
+            }
+        }
+        return null;
+    }
+}
