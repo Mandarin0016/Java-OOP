2d17a1f3581fc8d2b2be27af01789b57fd254051
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-25 23:48:51 +0300

    Exam Preparation

---
+++ Exam Preparation/FairyShopApp/src/main/java/core/ControllerImpl.java
@@ -0,0 +1,28 @@
+package fairyShop.core;
+
+public class ControllerImpl implements Controller {
+    @Override
+    public String addHelper(String type, String helperName) {
+        return null;
+    }
+
+    @Override
+    public String addInstrumentToHelper(String helperName, int power) {
+        return null;
+    }
+
+    @Override
+    public String addPresent(String presentName, int energyRequired) {
+        return null;
+    }
+
+    @Override
+    public String craftPresent(String presentName) {
+        return null;
+    }
+
+    @Override
+    public String report() {
+        return null;
+    }
+}
