2ff2ce62c6411df7cdcee9cd7fa607d05c7bac0b
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-28 18:48:10 +0300

    Unit Testing - Lab

--- Unit Testing - Lab/src/test/java/rpg_lab/AxeTest.java
+++ Unit Testing - Lab/src/test/java/rpg_lab/AxeTest.java
@@ -1,21 +1,36 @@
 package rpg_lab;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AxeTest {
 
+    private static final int AXE_ATTACK = 100;
+    private static final int AXE_DURABILITY = 10;
+    private static final int AXE_BROKEN_DURABILITY = 0;
+    private static final int DUMMY_HEALTH = 100;
+    private static final int DUMMY_EXP = 15;
+    private static Axe axe;
+    private static Axe brokenAxe;
+    private static Dummy dummy;
+
+    @Before
+    public void setup() {
+        axe = new Axe(AXE_ATTACK, AXE_DURABILITY);
+        brokenAxe = new Axe(AXE_ATTACK, AXE_BROKEN_DURABILITY);
+        dummy = new Dummy(DUMMY_HEALTH, DUMMY_EXP);
+    }
+
     @Test
-    public void testAxeLoseDurabilityAfterAttack(){
-        Axe axe = new Axe(100, 10);
-        axe.attack(new Dummy(100, 50));
-        Assert.assertEquals(9 , axe.getDurabilityPoints());
+    public void testAxeLoseDurabilityAfterAttack() {
+        axe.attack(dummy);
+        Assert.assertEquals(AXE_DURABILITY - 1, axe.getDurabilityPoints());
     }
 
     @Test(expected = IllegalStateException.class)
-    public void testAttackWithBrokenWeapon(){
-        Axe axe = new Axe(10, 0);
-        axe.attack(new Dummy(10, 10));
+    public void testAttackWithBrokenWeapon() {
+        brokenAxe.attack(dummy);
     }
 
 }
