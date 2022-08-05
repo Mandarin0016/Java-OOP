e6b719554d52053359e463dfa2fad77ea8381e22
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-08-05 17:58:59 +0300

    Exam Preparation

---
+++ Exam Preparation/restaurant/src/main/java/restaurant/entities/drinks/Smoothie.java
@@ -0,0 +1,10 @@
+package restaurant.entities.drinks;
+
+public class Smoothie extends BaseBeverage {
+
+    private static final double smoothiePrice = 4.50;
+
+    public Smoothie(String name, int counter, String brand) {
+        super(name, counter, smoothiePrice, brand);
+    }
+}
